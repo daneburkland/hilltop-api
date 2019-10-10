21738b50c3b74cdab255ab671c90f89bf28e98cb
Dane Burkland <dane.burkland@centro-201716.centro.net>
2019-10-10 08:13:30 -0700

    sends down screenshot S3 references

--- services/recordingTasks/run.js
+++ services/recordingTasks/run.js
@@ -2,18 +2,32 @@ import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 import axios from "axios";
 import AWS from "aws-sdk";
+import uuid from "uuid";
+
+async function putObjectToS3({ key, data, bucket }) {
+  var s3 = new AWS.S3();
+  var params = {
+    Bucket: bucket,
+    Key: key,
+    Body: new Buffer(data)
+  };
+  await s3.putObject(params).promise();
+  return { bucket, key };
+}
 
 export async function main(event, context) {
-  console.log("event records:", event.Records.length);
+  console.log("event records:", event.Records);
   const recordingsToRun = event.Records.filter(r =>
     ["MODIFY", "INSERT"].includes(r.eventName)
   ).map(async ({ dynamodb }) => {
     const recording = AWS.DynamoDB.Converter.unmarshall(dynamodb.NewImage);
     const code = recording.testCode;
 
-    let result;
+    // TODO: this should only run if 'active' is set
+    let result, screenshots, resolvedScreenshots;
+    const resultId = uuid.v1();
     try {
-      console.log("trying to run function");
+      console.log("trying to run function:", recording);
       result = await axios({
         method: "POST",
         url: `${process.env.hilltopChromeUrl}/function`,
@@ -24,11 +38,21 @@ export async function main(event, context) {
         },
         data: { code }
       });
+      console.log("ran function");
+      screenshots = result.data.screenshots.map(({ data }, i) =>
+        putObjectToS3({
+          bucket: process.env.screenshotsBucket,
+          key: `${recording.noteId}-${resultId}-${i}`,
+          data
+        })
+      );
+
+      resolvedScreenshots = await Promise.all(screenshots);
+      console.log("screenshots", resolvedScreenshots);
     } catch (err) {
+      console.log("save error", err);
       result = err;
     }
-
-    console.log(typeof result, result);
 
     const params = {
       TableName: process.env.recordingTableName,
@@ -46,12 +70,15 @@ export async function main(event, context) {
           {
             status: result.status,
             statusText: result.statusText,
-            headers: result.headers
+            headers: result.headers,
+            screenshots: resolvedScreenshots
           }
         ],
         ":empty_list": []
       }
     };
+
+    console.log("params defined");
 
     try {
       console.log("trying to save");
