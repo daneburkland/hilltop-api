5c24f82f768caf82ff3f780f2ab1253a29cb6f62
Dane Burkland <dane.burkland@centro-201716.centro.net>
2019-10-11 08:57:55 -0700

    rendering screenshots

--- services/recordingTasks/run.js
+++ services/recordingTasks/run.js
@@ -7,8 +7,10 @@ import uuid from "uuid";
 async function putObjectToS3({ key, data, bucket }) {
   var s3 = new AWS.S3();
   var params = {
+    // TODO: this should prob be 'authenticated-read'?
+    ACL: "public-read-write",
     Bucket: bucket,
-    Key: key,
+    Key: `public/${key}`,
     Body: new Buffer(data)
   };
   await s3.putObject(params).promise();
